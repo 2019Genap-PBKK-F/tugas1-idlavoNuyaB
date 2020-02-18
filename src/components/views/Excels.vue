99d24d7f13dc977785eac81fb18174949bc190c9
idlavoNuyaB <36811359+idlavoNuyaB@users.noreply.github.com>
2020-02-18 22:13:02 +0700

    with db and update and insert

--- src/components/views/Excels.vue
+++ src/components/views/Excels.vue
@@ -10,19 +10,91 @@
 <script>
 import jexcel from 'jexcel'
 import 'jexcel/dist/jexcel.css'
+import axios from 'axios'
 
-var data = [
-  ['05111740000085', 'Bayu Novaldi', '2017', 'Laki-Laki', '04/11/1998', '', true]
-]
+var changed = function(instance, cell, x, y, value) {
+  x = parseInt(x)
+  y = parseInt(y)
+  var datatemp = []
+  datatemp[0] = y + 1
+  axios.get('http://localhost:3000/mahasiswa/' + datatemp[0]).then((response) => {
+    Object.keys(response.data).map(function (key) {
+      if (key === 'nama') {
+        datatemp[2] = response.data['nama']
+      }
+      if (key === 'nrp') {
+        datatemp[1] = response.data['nrp']
+      }
+      if (key === 'angkatan') {
+        datatemp[3] = response.data['angkatan']
+      }
+      if (key === 'jeniskelamin') {
+        datatemp[4] = response.data['jeniskelamin']
+      }
+      if (key === 'tgllahir') {
+        datatemp[5] = response.data['tgllahir']
+      }
+      if (key === 'photo') {
+        datatemp[6] = response.data['photo']
+      }
+      if (key === 'aktif') {
+        datatemp[7] = response.data['aktif']
+      }
+    })
+    datatemp[x] = value
+    axios({
+      method: 'put',
+      url: 'http://localhost:3000/mahasiswa/' + datatemp[0],
+      data: {
+        id: datatemp[0],
+        nrp: datatemp[1],
+        nama: datatemp[2],
+        angkatan: datatemp[3],
+        jeniskelamin: datatemp[4],
+        tgllahir: datatemp[5],
+        photo: datatemp[6],
+        aktif: datatemp[7]
+      }
+    }).then((response) => {
+      console.log(response.data)
+    })
+  })
+}
+
+var insertrow = function(instance) {
+  axios({
+    method: 'post',
+    url: 'http://localhost:3000/mahasiswa/',
+    data: {
+    }
+  }).then((response) => {
+    console.log(response.data)
+  })
+}
+
+var deleterow = function(instance) {
+  axios({
+    method: 'delete',
+    url: 'http://localhost:3000/mahasiswa/',
+    data: {
+    }
+  }).then((response) => {
+    console.log(response.data)
+  })
+}
 
 var options = {
-  data: data,
+  url: 'http://localhost:3000/mahasiswa',
+  onchange: changed,
+  oninsertrow: insertrow,
+  ondeleterow: deleterow,
   allowToolbar: true,
   columns: [
+    { type: 'hidden', title: 'id', width: '120px' },
     { type: 'text', title: 'NRP', width: '120px' },
     { type: 'text', title: 'Nama', width: '120px' },
     { type: 'text', title: 'Angkatan', width: '120px' },
-    { type: 'dropdown', title: 'Jenis Kelamin', width: '250px', source: ['Laki-Laki', 'Perempuan'] },
+    { type: 'dropdown', title: 'Jenis Kelamin', width: '250px', autocomplete: true, source: ['Laki-Laki', 'Perempuan'] },
     { type: 'calendar', title: 'Tgl-Lahir', width: '250px' },
     { type: 'image', title: 'Photo', width: '120px' },
     { type: 'checkbox', title: 'Aktif', width: '80px' }
@@ -38,8 +110,13 @@ var options = {
 export default {
   name: 'App',
   mounted: function () {
-    let spreadsheet = jexcel(this.$el, options)
-    Object.assign(this, { spreadsheet })
+    this.load()
+  },
+  methods: {
+    load() {
+      let spreadsheet = jexcel(this.$el, options)
+      Object.assign(this, { spreadsheet })
+    }
   }
 }
 </script>
